df6025a3962cb8c6a45f61b58f0dc6cb8c18cf7f
q7w7e7r <q7w7e7r@gmail.com>
2021-01-02 05:10:52 +0600

    Выделена фикстура и осуществлён переход на py.test

--- test_add_group.py
+++ test_add_group.py
@@ -5,7 +5,7 @@ from application import Application
 @pytest.fixture
 def app(request):
     fixture = Application()
-    request.addfinalizer(fixture.destroy())
+    # request.addfinalizer(fixture.destroy())
     return fixture
 
 
@@ -15,7 +15,7 @@ def test_add_group(app):
     app.logout()
 
 
-def test_empty_group(app):
+def test_add_empty_group(app):
     app.login(username="admin", password="secret")
     app.create_group(Group(name="", header="", footer=""))
     app.logout()
