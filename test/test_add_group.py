94ccbc57931296122852a824dc5c985ce248f034
q7w7e7r <q7w7e7r@gmail.com>
2021-01-02 14:28:05 +0600

    Выделение фикстуры в отдельный акласс application.py

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,10 +2,11 @@ import pytest
 from model.group import Group
 from fixture.application import Application
 
+
 @pytest.fixture
 def app(request):
     fixture = Application()
-    # request.addfinalizer(fixture.destroy())
+    request.addfinalizer(fixture.destroy)
     return fixture
 
 
