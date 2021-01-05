09ad0b8a2258ad37af830631fa002379e0c709db
q7w7e7r <q7w7e7r@gmail.com>
2021-01-05 21:39:25 +0600

    внесены изменения для работы с контактами, добавлен тест для сверки телефонов контакта на главной странице и страницы редактиврования

--- fixture/application.py
+++ fixture/application.py
@@ -2,15 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
+from fixture.contact import ContactHelper
 
 
 # Fixture
 class Application:
     def __init__(self):
         self.wd = webdriver.Chrome()
-        self.vars = {}
+        # self.vars = {}
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
+        self.contact = ContactHelper(self)
 
     def is_valid(self):
         try:
---
+++ fixture/contact.py
@@ -0,0 +1,52 @@
+from model.contact import Contact
+from selenium.webdriver.common.by import By
+
+
+class ContactHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element(By.NAME, field_name).click()
+            wd.find_element(By.NAME, field_name).clear()
+            wd.find_element(By.NAME, field_name).send_keys(text)
+
+    contact_cache = None
+
+    def get_contact_list(self):
+        if self.contact_cache is None:
+            wd=self.app.wd
+            self.app.open_home_page()
+            self.contact_cache = []
+            for row in wd.find_elements(By.NAME, "entry"):
+                cells = row.find_elements(By.TAG_NAME, "td")
+                firstname = cells[2].text
+                lastname = cells[1].text
+                id = cells[0].find_element(By.TAG_NAME, "input").get_attribute("value")
+                all_phones = cells[5].text.splitlines()
+                self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, id=id,
+                                                  homephone=all_phones[0],mobilephone=all_phones[1],
+                                                  workphone=all_phones[2]))
+        return list(self.contact_cache)
+
+    def open_contact_to_edit_by_index(self, index):
+        wd = self.app.wd
+        self.app.open_home_page
+        row = wd.find_elements(By.NAME, "entry")[index]
+        cell = row.find_elements(By.TAG_NAME,"td")[7]
+        cell.find_element(By.TAG_NAME, "a").click()
+
+    def get_contact_info_from_edit_page(self, index):
+        wd = self.app.wd
+        self.open_contact_to_edit_by_index(index)
+        firstname = wd.find_element(By.NAME, "firstname").get_attribute("value")
+        lastname = wd.find_element(By.NAME, "lastname").get_attribute("value")
+        id = wd.find_element(By.NAME, "id").get_attribute("value")
+        homephone = wd.find_element(By.NAME, "home").get_attribute("value")
+        workphone = wd.find_element(By.NAME, "work").get_attribute("value")
+        mobilephone = wd.find_element(By.NAME, "mobile").get_attribute("value")
+        return Contact(firstname=firstname,lastname=lastname,id=id,homephone=homephone,
+                       mobilephone=mobilephone,workphone=workphone)
