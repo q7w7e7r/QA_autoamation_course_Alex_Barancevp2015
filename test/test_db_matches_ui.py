ffa8dc4215afe2ca339bf82f93158e616d3be409
q7w7e7r <q7w7e7r@gmail.com>
2021-01-11 01:13:29 +0600

    Загрузка списка групп из БД Сравнение списка групп БД с UI

---
+++ test/test_db_matches_ui.py
@@ -0,0 +1,15 @@
+from model.group import Group
+from timeit import timeit
+
+def test_group_list(app,db):
+    # print (timeit(lambda:app.group.get_group_list(),number=1))
+    ui_list = app.group.get_group_list()
+    def clean(group):
+        return Group(id=group.id, name=group.name.strip())
+    db_list = map( clean ,db.get_group_list())
+    # print (timeit( lambda : map(clean ,db.get_group_list()),number=1))
+    assert sorted(ui_list,key=Group.id_or_max) == sorted(db_list,key=Group.id_or_max)
+
+    # ui_list = app.group.get_group_list()
+    # db_list = db.get_group_list()
+    # assert sorted(ui_list,key=Group.id_or_max) == sorted(db_list,key=Group.id_or_max)
