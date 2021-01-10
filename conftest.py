f1b4598e17129934a04d67da087984f2c65b154f
q7w7e7r <q7w7e7r@gmail.com>
2021-01-10 20:12:27 +0600

    Динамическое связывание тестовых функций с данными через фикстуру

--- conftest.py
+++ conftest.py
@@ -1,24 +1,41 @@
 import pytest
 import json
-from fixture.application import *
 import os.path
+import importlib
+from fixture.application import Application
+from fixture.db_f import DbFixture
+
+
 
 fixture = None
 target = None
 
-@pytest.fixture  # (scope="session")
-def app(request):
-    global fixture
+def load_config(file):
     global target
-    browser = request.config.getoption('--browser')
     if target is None:
-        config_file =  os.path.join(os.path.dirname(os.path.abspath(__file__)),request.config.getoption('--target'))
+        config_file =  os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
         with open(config_file) as f:
             target = json.load(f)
+    return target
+
+@pytest.fixture   (scope="session")
+def app(request):
+    global fixture
+    browser = request.config.getoption('--browser')
+    web_config = load_config( request.config.getoption('--target'))['web']
     if fixture is None or fixture.is_valid():
-        fixture = Application(browser=browser,base_url=target['baseUrl'])  # создание фикстуры
-    fixture.session.ensure_login(username=target['username'], password=target['password'])
+        fixture = Application(browser=browser,base_url=web_config['baseUrl'])  # создание фикстуры
+    fixture.session.ensure_login(username=web_config['username'], password=web_config['password'])
     return fixture
+
+@pytest.fixture(scope="session")
+def db(request):
+    db_config = load_config( request.config.getoption('--target'))['db']
+    dbfixture = DbFixture(host=db_config['host'],name=db_config['name'],user=db_config['user'],password=db_config['password'])
+    def fin():
+        dbfixture.destroy()
+    request.addfinalizer(fin)
+    return dbfixture
 
 @pytest.fixture(scope="session", autouse=True)
 def stop(request):
@@ -28,6 +45,20 @@ def stop(request):
     request.addfinalizer(fin)  # teardown фикстуры
     return fixture
 
+@pytest.fixture
+def check_ui(request):
+    request.config.getoption('--check_ui')
+
 def pytest_addoption(parser):
     parser.addoption('--browser', action='store', default='chrome' )
     parser.addoption('--target', action='store', default='target.json')
+    parser.addoption('--check_ui', action='store_true' )
+
+def pytest_generate_tests(metafunc):
+    for fixture in metafunc.fixturenames:
+        if fixture.startswith("data_"):
+            testdata = load_from_module(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+
+def load_from_module(module):
+    return importlib.import_module("data.%s" %  module).testdata
---
+++ data/groups.py
@@ -0,0 +1,8 @@
+from model.group import Group
+
+
+testdata = [
+    Group(name='name 1',header='header 1',footer='footer 1'),
+    Group(name='name 2',header='header 2',footer='footer 2')
+]
+
